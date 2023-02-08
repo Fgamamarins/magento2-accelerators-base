4a2a4704220163c9955c9193c7f3739d72aa4a53
Fausto Marins <fausto.junior@fcamara.com.br>
2023-02-08 18:09:30 -0300

    [Base] - Base Module v1

---
+++ registration.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to https://www.fcamara.com.br/ for more information.
+ *
+ * @Agency    Black Soul Foda. (http://www.blacksoulfoda.com.br)
+ * @author    Fausto Marins <faustogmjr@gmail.com>
+ * @co-author Yan Rocha <seila@gmail.com>
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'BlackSoul_Base', __DIR__);
